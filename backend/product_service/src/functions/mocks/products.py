1f06e558ac4c2fe33173b69f0e6eb51c19cc372f
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2025-02-20 17:39:09 -0500

    Change products mocks and openapi.yaml

--- backend/product_service/src/functions/mocks/products.py
+++ backend/product_service/src/functions/mocks/products.py
@@ -1,16 +1,38 @@
 products = [
     {
-        "id": "1",
-        "title": "My Product 1",
-        "description": "Description for product 1",
-        "price": 99.99,
-        "count": 10
+        "description": "Short Product Description1",
+        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+        "price": 24,
+        "title": "ProductOne",
     },
     {
-        "id": "2",
-        "title": "My Product 2",
-        "description": "Description for product 2",
-        "price": 149.99,
-        "count": 5
-    }
+        "description": "Short Product Description7",
+        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+        "price": 15,
+        "title": "ProductTitle",
+    },
+    {
+        "description": "Short Product Description2",
+        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+        "price": 23,
+        "title": "Product",
+    },
+    {
+        "description": "Short Product Description4",
+        "id": "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+        "price": 15,
+        "title": "ProductTest",
+    },
+    {
+        "description": "Short Product Descriptio1",
+        "id": "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+        "price": 23,
+        "title": "Product2",
+    },
+    {
+        "description": "Short Product Description7",
+        "id": "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+        "price": 15,
+        "title": "ProductName",
+    },
 ]
